4894dd321ec0313c12cd1b3a4c19800cb09d194a
Golik07 <126725620+Golik07@users.noreply.github.com>
2023-10-28 13:07:00 +0200

    update

---
+++ dist/concatenation.js
@@ -0,0 +1,5 @@
+function concatenation(firstWrod, secondWord) {
+    console.log(`${firstWrod} ${secondWord}`);
+}
+export { concatenation };
+//# sourceMappingURL=concatenation.js.map
---
+++ src/concatenation.ts
@@ -0,0 +1,5 @@
+function concatenation(firstWrod: string, secondWord: string) {
+  console.log(`${firstWrod} ${secondWord}`);
+}
+
+export { concatenation };
